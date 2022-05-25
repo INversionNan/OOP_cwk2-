a54a57b190528e2b62e0f31d8d2ca6861c9ad42a
10526 <sc202yc@leeds.ac.uk>
2022-05-26 03:00:24 +0800

    Finish the basic implementation and the Deck class to override some functions

--- cwk2/src/main/java/comp1721/cwk2/Deck.java
+++ cwk2/src/main/java/comp1721/cwk2/Deck.java
@@ -1,3 +1,40 @@
 package comp1721.cwk2;
 
+import java.util.Collections;
+
+public class Deck extends CardCollection{
+
+    public Deck(){
+        for (Card.Rank rank: Card.Rank.values()){
+            for(Card.Suit suit: Card.Suit.values()){
+                Card card = new Card(rank, suit);
+                super.add(card);
+            }
+        }
+    }
+
+    @Override
+    public int size(){
+        return super.size();
+    }
+
+    @Override
+    public boolean isEmpty(){
+        return super.isEmpty();
+    }
+
+    @Override
+    public boolean contains(Card card){
+        return super.contains(card);
+    }
+
+    @Override
+    public void discard() {
+        super.discard();
+    }
+
+    public void shuffle(){
+        Collections.shuffle(super.cards);
+    }
+}
 // Implement Deck class here
--- cwk2/src/main/java/comp1721/cwk2/PokerHand.java
+++ cwk2/src/main/java/comp1721/cwk2/PokerHand.java
@@ -1,3 +1,52 @@
 package comp1721.cwk2;
 
+public class PokerHand {
+    public PokerHand(){
+
+    }
+
+    public PokerHand(String name){
+        if (name.length() <= 0 || name.length() > 5){
+            throw new CardException("These card numbers are wrong.");
+        }
+        for ()
+    }
+//    @Override
+//    public boolean equals(Object obj) {
+//        return super.equals(obj);
+//    }
+
+//    @Override
+//    protected Object clone() throws CloneNotSupportedException {
+//        return super.clone();
+//    }
+
+    @Override
+    public String toString() {
+
+    }
+
+    public boolean isPair(){
+
+    }
+
+    public boolean isTwoPairs(){
+
+    }
+    public boolean isThreeOfAKind(){
+
+    }
+    public boolean isFourOfAKind(){
+
+    }
+    public boolean isFullHouse(){
+
+    }
+    public boolean isFlush(){
+
+    }
+    public boolean isStraight(){
+
+    }
+}
 // Implement PokerHand class here
